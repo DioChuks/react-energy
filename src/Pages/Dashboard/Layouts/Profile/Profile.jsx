9ad46b38203c2c82478af087aae055330354d4fd
DioChuks <diochuks65@gmail.com>
2024-01-10 11:31:32 +0100

    API UPDATE

--- src/Pages/Dashboard/Layouts/Profile/Profile.jsx
+++ src/Pages/Dashboard/Layouts/Profile/Profile.jsx
@@ -1,8 +1,16 @@
 
 function Profile() {
     return(
-        <div className="profile-section">
-            Here's your profile!
+        <div className="profile">
+            <div className="profile-box">
+                <div className="profile-heading">
+                    <h2 className="profile-title">Here's your profile!</h2>
+                    <p className="profile-subtitle">Feel free to update it</p>
+                </div>
+                <div className="profile-form-box">
+                    
+                </div>
+            </div>
         </div>
     );
 }
--- src/routes/api.jsx
+++ src/routes/api.jsx
@@ -1,1 +1,1 @@
-export const apiUrl = 'http://localhost:8000/api';
+export const apiUrl = 'https://ecohavest-api.onrender.com/api';
